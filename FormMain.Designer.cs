df428ad48e058e1c18e2c252c79a470ab533f6a3
wberdowski <10144015+wberdowski@users.noreply.github.com>
2020-03-15 22:33:20 +0100

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SpectrumAnalyzer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(798, 507);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 507);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(798, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 529);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SpectrumAnalyzer";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,94 @@
+﻿using CSCore.DSP;
+using CSCore.SoundIn;
+using System;
+using System.Windows.Forms;
+
+namespace SpectrumAnalyzer
+{
+    public partial class FormMain : Form
+    {
+        private FftProvider fftProvider;
+        private WasapiLoopbackCapture wasapi;
+        private const int numBars = 1024 * 8;
+        private const int density = numBars / (1024 * 2);
+        private const int bytesPerPoint = 4;
+        private int sampleRate;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            Application.ApplicationExit += Application_ApplicationExit;
+
+            // Setup chart
+
+            for (int i = 0; i < numBars / density; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, 0);
+            }
+            chart1.ChartAreas[0].AxisY.Maximum = 60;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 20000;
+
+            fftProvider = new FftProvider(1, (FftSize)numBars);
+
+            wasapi = new WasapiLoopbackCapture(10);
+            wasapi.DataAvailable += Wasapi_DataAvailable;
+            wasapi.Initialize();
+            wasapi.Start();
+
+            // Status
+            var format = wasapi.WaveFormat;
+            sampleRate = format.SampleRate;
+            toolStripStatusLabel1.Text = $"WaveFormat: {format.SampleRate}Hz, {format.BitsPerSample} bits, {format.Channels} channel(s)";
+        }
+
+        private void Wasapi_DataAvailable(object sender, DataAvailableEventArgs e)
+        {
+            int graphPoints = e.ByteCount / bytesPerPoint;
+            float[] pcm = new float[graphPoints];
+            float[] fft = new float[graphPoints];
+            float[] fftReal = new float[graphPoints / 2];
+
+            double pcmPointSpacingMs = sampleRate / (density / 2);
+
+            for (int i = 0; i < graphPoints; i++)
+            {
+                var val = BitConverter.ToSingle(e.Data, i * bytesPerPoint);
+                pcm[i] = val; // * FastFourierTransformation.HammingWindowF(i, graphPoints);
+            }
+
+            fftProvider.Add(pcm, pcm.Length);
+
+            float[] fftBuffer = new float[numBars];
+
+            if (fftProvider.GetFftData(fftBuffer))
+            {
+                try
+                {
+                    if (!IsDisposed && !Disposing)
+                        Invoke((MethodInvoker)delegate ()
+                        {
+                            for (int i = 0; i / (float)(numBars / density) * pcmPointSpacingMs < 20000; i++)
+                            {
+                                float amplitude = fftBuffer[i] * 200;
+                                chart1.Series[0].Points[i].SetValueXY(i / (float)(numBars / density) * pcmPointSpacingMs, amplitude);
+                            }
+                            chart1.Invalidate();
+                        });
+                }
+                catch (ObjectDisposedException)
+                {
+
+                }
+            }
+        }
+
+        private void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            wasapi.DataAvailable -= Wasapi_DataAvailable;
+            wasapi.Stop();
+            wasapi.Dispose();
+        }
+    }
+}
